87bde24d179b883c033b7b5baef21acfcfda4e88
miam1gh0st <1992584620@nbt.edu.cn>
2024-10-08 20:09:27 +0800

    write a git test

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "22122aaac94e9280"; }
+/******/ 		__webpack_require__.h = function() { return "52c5cab46a354521"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ pages/test4freshman.md
@@ -0,0 +1,1 @@
+# this is a test for freshman to use git.
